e8510a395a0d2857d26fd05046355f3c7ae9ce10
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-11-28 18:00:58 +0545

    added django debug environ

--- src/sdhome/settings.py
+++ src/sdhome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wr@bt#%iuax+4avpg)ptm+we%$v_8486$o+&1=n7g5)xu4vs@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")).lower() == "true" or True
 
 ALLOWED_HOSTS = [
     ".railway.app" 
