49e84806483da60f6290967154c811450912c419
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-11-29 12:27:25 +0545

    finished learning neon in procduction and database branching

--- src/sdhome/settings.py
+++ src/sdhome/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true" or True
 DEBUG = config("DJANGO_DEBUG", cast=bool)
-print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" 
@@ -90,6 +89,20 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASE_URL = config("DATABASE_URL", cast=str)
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default = 30)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL, 
+        conn_health_checks=True, 
+        conn_max_age=CONN_MAX_AGE
+        )
+
+}
+
 
 
 # Password validation
